48fe741f9a2bba158f30d4349eb308d88d75b8af
hungtvb <69663872+hungtvb@users.noreply.github.com>
2026-07-23 08:47:09 +0700

    feat(api): load current votes in batches

--- src/main/java/com/hungtvb/votesystem/vote/VoteRepository.java
+++ src/main/java/com/hungtvb/votesystem/vote/VoteRepository.java
@@ -1,9 +1,14 @@
 package com.hungtvb.votesystem.vote;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface VoteRepository extends JpaRepository<Vote, UUID> {
     Optional<Vote> findByUserIdAndPostId(UUID userId, UUID postId);
+
+    List<Vote> findByUserIdAndPostIdIn(UUID userId, Collection<UUID> postIds);
 }
