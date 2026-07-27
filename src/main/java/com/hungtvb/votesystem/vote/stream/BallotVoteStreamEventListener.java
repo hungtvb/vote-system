feaf4c28d72d1511ff57eb038db2f259d72df082
hungtvb <69663872+hungtvb@users.noreply.github.com>
2026-07-26 20:20:35 -0700

    TON-130 time SSE listener latency

--- src/main/java/com/hungtvb/votesystem/vote/stream/BallotVoteStreamEventListener.java
+++ src/main/java/com/hungtvb/votesystem/vote/stream/BallotVoteStreamEventListener.java
@@ -1,19 +1,24 @@
 package com.hungtvb.votesystem.vote.stream;
 
+import com.hungtvb.votesystem.vote.metrics.VoteLatencyMetrics;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
+import static com.hungtvb.votesystem.vote.metrics.VoteLatencyMetrics.OPERATION_POST_COMMIT;
+
 @Component
 public class BallotVoteStreamEventListener {
     private final BallotVoteStreamService streamService;
+    private final VoteLatencyMetrics metrics;
 
-    public BallotVoteStreamEventListener(BallotVoteStreamService streamService) {
+    public BallotVoteStreamEventListener(BallotVoteStreamService streamService, VoteLatencyMetrics metrics) {
         this.streamService = streamService;
+        this.metrics = metrics;
     }
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void onVoteChanged(BallotVoteChangedEvent event) {
-        streamService.publish(event.update());
+        metrics.timeStage(OPERATION_POST_COMMIT, "sse_listener", () -> streamService.publish(event.update()));
     }
 }
