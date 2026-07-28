c4129071629761d6ce140e1a9e3fffe76498089d
hungtvb <69663872+hungtvb@users.noreply.github.com>
2026-07-27 19:13:41 -0700

    TON-108 add profile preset enums

---
+++ src/main/java/com/hungtvb/votesystem/user/AvatarIcon.java
@@ -0,0 +1,14 @@
+package com.hungtvb.votesystem.user;
+
+public enum AvatarIcon {
+    CITIZEN,
+    ADVOCATE,
+    THINKER,
+    ORGANIZER,
+    VOLUNTEER,
+    CREATOR,
+    LEADER,
+    ANALYST,
+    VISIONARY,
+    BUILDER
+}
